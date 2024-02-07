7772337c95509ead6caf756c0b4909d797bb5fdb
ahmad <ahmadsaeed8081@gmail.com>
2024-02-07 13:38:02 +0500

    third commit

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
-export const cont_add="0x5FD0e77776e4F7E2d9417C16fbcAcaD786fDF83F";
+export const cont_add="0x04A754546DeD81f6e35Ba1F93E1aF4cA3129F1E5";
 // export const cont_add="0xA170eD1D61e0ab415B740D5e8bC7D615E7b2A31E";
 
 
